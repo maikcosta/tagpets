e2c43562ee7c5fdf33b4ad9c79b1290e75df89bd
Maik Costa <maikcosta@gmail.com>
2023-03-31 13:08:04 -0300

    Inicializando projeto Spring-Boot

---
+++ src/main/java/com/tagpets/tagpets/TagpetsApplication.java
@@ -0,0 +1,13 @@
+package com.tagpets.tagpets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagpetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagpetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tagpets/tagpets/TagpetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tagpets.tagpets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagpetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
